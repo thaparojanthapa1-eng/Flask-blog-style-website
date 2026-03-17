b2c31ada6e982436ffd4bd03cc9239c3b86182a3
thaparojanthapa1-eng <thaparojanthapa1@gmail.com>
2026-03-17 19:54:43 +0545

    Moved flask initialization from app.py to __init.py

--- app.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from os import flask
-from flask_login import login_manager
-
-app=Flask(__name__)
---
+++ website/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import flask
+from flask_login import login_manager
+
+def create_app():
+    app=Flask(__name__)
+    app.config['SECRET_KEY']="Hello_world"
+    return app
