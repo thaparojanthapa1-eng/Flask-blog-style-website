baa387c9e4cbcf495511445424519de8f8f212e0
thaparojanthapa1-eng <thaparojanthapa1@gmail.com>
2026-03-18 19:01:41 +0545

    Initialised blueprint

--- website/__init__.py
+++ website/__init__.py
@@ -6,4 +6,11 @@ from flask_login import login_manager
 def create_app():
     app=Flask(__name__)
     app.config['SECRET_KEY']="Hello_world"
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
+
     return app
