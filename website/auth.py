c0e7074745434ed32f5b1077849f06412e6b2a11
thaparojanthapa1-eng <thaparojanthapa1@gmail.com>
2026-03-20 19:14:23 +0545

    Added proper handling for login and signup form inputs

--- website/auth.py
+++ website/auth.py
@@ -1,13 +1,19 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 
 auth=Blueprint("auth", __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET", "POST"])
 def login():
+    email=request.form("email")
+    password=request.form("password")
     return render_template("login.html")
 
-@auth.route("/signup")
+@auth.route("/signup", methods=["GET", "POST"])
 def signup():
+    username=request.form("username")
+    email=request.form("email")
+    password1=request.form("password1")
+    password2=request.form("password2")
     return render_template("signup.html")
 
 @auth.route("/logout")
