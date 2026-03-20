3df0e2e7f275e57dff6a20bef566c6d0e3254cc3
thaparojanthapa1-eng <thaparojanthapa1@gmail.com>
2026-03-20 19:29:55 +0545

    Created a function to create a database if database doesn't already exist

--- website/__init__.py
+++ website/__init__.py
@@ -18,4 +18,11 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
+    create_database(app)
+
     return app
+
+def create_database(app):
+    if not path.exists("website/"+DB_NAME):
+        db.create_all(app=app)
+        print("Created database!")
