5430cd92c4c6f5b9d1281d646d2372bac24ca5c8
thaparojanthapa1-eng <thaparojanthapa1@gmail.com>
2026-03-22 19:25:15 +0545

    Added login data to the database

--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,28 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from . import Db
 from .models import User
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth=Blueprint("auth", __name__)
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
-    email=request.form("email")
-    password=request.form("password")
-    return render_template("login.html")
+    if request.method=="POST":
+        email=request.form("email")
+        password=request.form("password")
+        return render_template("login.html")
+    
+        user=User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Logged in!")
+                login_user(user, remember=True)
+                return redirect(url_for("views.home"))
+            else:
+                flash("Password is incorrect.", category=error)
+        else:
+            flash("User doesn't exist.", category=error)
 
 @auth.route("/signup", methods=["GET", "POST"])
 def signup():
@@ -33,8 +47,13 @@ def signup():
         elif len(email)<10:
             flash("Email too short", category="error")
         else:
-            new_user=User(email=email, username=username, password=password1)
+            new_user=User(email=email, username=username, 
+                          password=generate_password_hash(password1, method="sha256"))
+            Db.session.add(new_user)
             Db.session.commit()
+            login_user(new_user, remember=True)
+            flash("User created")
+            return (redirect(url_for("views.home")))
 
     return render_template("signup.html")
 
