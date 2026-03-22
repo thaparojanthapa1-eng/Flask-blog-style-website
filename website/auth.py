6dd3acddd8805caa0a9ddef8c9949b21b8cb4b73
thaparojanthapa1-eng <thaparojanthapa1@gmail.com>
2026-03-22 19:02:59 +0545

    Added user to database

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, redirect, url_for, request
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from . import Db
+from .models import User
 
 auth=Blueprint("auth", __name__)
 
@@ -10,10 +12,30 @@ def login():
 
 @auth.route("/signup", methods=["GET", "POST"])
 def signup():
-    username=request.form("username")
-    email=request.form("email")
-    password1=request.form("password1")
-    password2=request.form("password2")
+    if request.method=="POST":
+        username=request.form("username")
+        email=request.form("email")
+        password1=request.form("password1")
+        password2=request.form("password2")
+
+        email_exist=User.query.filter_by(email=email).first
+        username_exist=User.query.filter_by(username=username).first
+        if email_exist:
+            flash("Email is already in use.", category="error")
+        elif username_exist:
+            flash("Username already in use.", category="error")
+        elif password1!=password2:
+            flash("Password doesn't match", category="error")
+        elif len(username)<2:
+            flash("Username too short", category="error")
+        elif len(password1)<2:
+            flash("Password too short", category="error")
+        elif len(email)<10:
+            flash("Email too short", category="error")
+        else:
+            new_user=User(email=email, username=username, password=password1)
+            Db.session.commit()
+
     return render_template("signup.html")
 
 @auth.route("/logout")
